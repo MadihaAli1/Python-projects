d143fbd8c886bebef7a0f5bf36abd0a020d3b30d
MadihaAli1 <madihmali57@gmail.com>
2025-03-02 03:28:13 +0500

    add project

--- 01-Growth-Mindset-Challenge/app.py
+++ 01-Growth-Mindset-Challenge/app.py
@@ -6,7 +6,7 @@ st.set_page_config(page_title="File Converter", layout="wide")
 st.title("File Converter & Cleaner")
 st.write("Upload CSV or Excel files, clean and convert them into other formats")
 
-files = st.file_uploader("Upload CSV or Excel files", type=["csv", "xlsx"], accept_multiple_files=True)
+files = st.file_uploader("Upload CSV or Excel files", type=["csv", "xlsx"], accept_multiple_files= True)
 
 if files:
     for file in files:
